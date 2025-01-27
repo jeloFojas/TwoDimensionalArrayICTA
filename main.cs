6db2902982b0af904a647134cc63e4e8dc4cc7d0
jeloFojas <jelo.fojas@deped.gov.ph>
2025-01-27 09:42:59 +0000

    2nd commit

--- main.cs
+++ main.cs
@@ -2,6 +2,24 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+
+    
+    string [,] data = new string [5,2];    
+    
+    string [,] studentData = {
+                                {"TONGOL", "MOSES"},
+                                {"MAUNA", "HIZZAM"},
+                                {"ABRILLO","LOWELL"},
+                                {"CAVILLE","SHYRA"},
+                                {"MONTEVERDE","GIRALENE"}        
+                                };
+
+    // studentData[2,0] = "CURRACHEA";
+    // studentData[2,1] = "SHANE";
+
+    Console.WriteLine(studentData[2,0]+studentData[2,1]);
+    
+
+    
   }
 }
